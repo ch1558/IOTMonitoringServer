e2ab3ae5240fec83beb385bcba0d99e7d1e8e91e
ch1558 <juancamilohp@ufps.edu.co>
2024-09-16 23:46:03 -0500

    update second query

--- control/monitor.py
+++ control/monitor.py
@@ -34,11 +34,20 @@ def analyze_data():
                 'station__location__country__name')
     print("data original" + str(data.count()))
     
-    data_temp = Data.objects.filter(measurement__name="Temperatura") \
+    data_temp = Data.objects.filter(measurement__name="temperatura") \
         .select_related('station__user', 'station__location') \
-        .values('time', 'base_time', 'station_id', 'measurement_id', 'min_value', 'max_value', 'length', 'avg_value', 'times', 'values')
+        .select_related('station__location__city', 'station__location__state',
+                        'station__location__country') \
+        .values('check_value', 
+                'station__user__username',
+                'measurement__name',
+                'measurement__max_value',
+                'measurement__min_value',
+                'station__location__city__name',
+                'station__location__state__name',
+                'station__location__country__name')
     print("poc_element")
-    print(str(data_temp.count()))
+    print( str(data_temp.count()))
     
     alerts = 0
     for item in aggregation:
